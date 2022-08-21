9a7956156c5bc6169e127d70cf60698c896edade
aritraroy26 <aritra.roy.cse.26@gmail.com>
2022-08-21 15:04:12 +0530

    basic project setup

---
+++ src/fake-data.js
@@ -0,0 +1,21 @@
+const USER_ID_1 = "2uMrJZJmsUUQfrZmYjewCCNHku13";
+const USER_ID_2 = "QA259l2MPwdnTWWeFx1x4YrqxLK2 ";
+const USER_ID_3 = "VNxmwr1ghJOJbaQjt2NxmrmK5Qp1";
+
+module.exports.users = [
+  {
+    id: USER_ID_1,
+    email: "mimi@gmail.com",
+    fullName: "Mimi Das",
+  },
+  {
+    id: USER_ID_2,
+    email: "aparna@gmail.com",
+    fullName: "Aparna Rpy",
+  },
+  {
+    id: USER_ID_3,
+    email: "samir@gmail.com",
+    fullName: "Samir Roy",
+  },
+];
